8d81045429d972489913f0a5444547c7c767d58e
Eder Rafo Jose Pariona Espinhal <ederrafo@gmail.com>
2019-11-10 12:22:49 -0500

    added Hello world on project

--- src/main/java/com/ederrafo/controller/HelloWorldController.java
+++ src/main/java/com/ederrafo/controller/HelloWorldController.java
@@ -5,14 +5,16 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-// Con esta anotacion la haceos un bean de spring boot, es decir deja de ser una clase comun de java
 @Controller
+// Request mapping a nivel de clase
 @RequestMapping("/say")
 public class HelloWorldController {
-
+    //Reques a nivel de metodo
     @GetMapping("/helloworld")
     public String helloWorld(){
         return "helloWorld";
     }
 
 }
+
+// Con esta anotacion la haceos un bean de spring boot, es decir deja de ser una clase comun de java
