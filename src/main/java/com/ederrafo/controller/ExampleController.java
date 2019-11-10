f4691f84fbf883462747122380a3abed72de7565
Eder Rafo Jose Pariona Espinhal <ederrafo@gmail.com>
2019-11-10 18:57:45 -0500

    inserting simple data in templates

---
+++ src/main/java/com/ederrafo/controller/ExampleController.java
@@ -0,0 +1,35 @@
+package com.ederrafo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("/example")
+public class ExampleController {
+
+    public static final String EXAMPLE_VIEW = "example";
+    // Primera forma
+    // @RequestMapping(value = "/exampleString", method = RequestMethod.GET)
+    @GetMapping("/exampleString")
+    // Necesitamos un model para pasarle parametros
+    public String exampleString(Model model){
+        // Si el model viene vacio entonces lo crea
+        model.addAttribute("name", "Eder");
+
+        return EXAMPLE_VIEW;
+    }
+
+    // Segunda forma
+    // @RequestMapping(value = "/exampleMAV", method = RequestMethod.GET)
+    @GetMapping("/exampleMAV")
+    public ModelAndView exampleMAV(){
+        ModelAndView model = new ModelAndView(EXAMPLE_VIEW);
+        model.addObject("name", "Rafo");
+
+        return model;
+    }
+}
