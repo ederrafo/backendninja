4d1aebe536b1517459fb89f821ebfbcab9f60def
Eder Rafo Jose Pariona Espinhal <ederrafo@gmail.com>
2019-11-10 19:51:34 -0500

    iterating list in templates

--- src/main/java/com/ederrafo/controller/ExampleController.java
+++ src/main/java/com/ederrafo/controller/ExampleController.java
@@ -8,11 +8,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
 @RequestMapping("/example")
 public class ExampleController {
 
     public static final String EXAMPLE_VIEW = "example";
+    public static final String TEMPLATE_PEOPLES = "peoples";
+
     // Primera forma
     // @RequestMapping(value = "/exampleString", method = RequestMethod.GET)
     @GetMapping("/exampleString")
@@ -32,5 +37,31 @@ public class ExampleController {
         model.addObject("person",  new Person("Rafo", 34));
 
         return model;
+    }
+
+    @GetMapping("/peoples")
+    public ModelAndView peoples(){
+        ModelAndView model = new ModelAndView(TEMPLATE_PEOPLES);
+        model.addObject("peoples",  getPeoples());
+
+        String nameofCurrMethod = new Throwable()
+                .getStackTrace()[0]
+                .getMethodName();
+
+        System.out.println("Name of current method: "
+                + nameofCurrMethod);
+
+        return model;
+    }
+
+    private List<Person> getPeoples(){
+        List<Person> peoples = new ArrayList<>();
+        peoples.add(new Person("Eder", 34));
+        peoples.add(new Person("Rafo", 33));
+        peoples.add(new Person("Oliver", 43));
+        peoples.add(new Person("Ana", 24));
+
+        return peoples;
+
     }
 }
