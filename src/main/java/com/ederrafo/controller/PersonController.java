5cd56e72b9d38c8ef108ca980a8807c656581cd2
Eder Rafo Jose Pariona Espinhal <ederrafo@gmail.com>
2019-11-17 16:36:27 -0500

    added logs apache commons for our application

--- src/main/java/com/ederrafo/controller/PersonController.java
+++ src/main/java/com/ederrafo/controller/PersonController.java
@@ -1,6 +1,8 @@
 package com.ederrafo.controller;
 
 import com.ederrafo.model.Person;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,20 +16,29 @@ import org.springframework.web.servlet.ModelAndView;
 @RequestMapping("/person")
 public class PersonController {
 
+    private static final Log LOGGER = LogFactory.getLog(PersonController.class);
+
     @GetMapping("/add")
     public String add(Model model)
     {
         // le pasamos un objecto vacio
         model.addAttribute("person", new Person());
-        int i = 90/0;
+
+        // test log trace
+        LOGGER.info("Info trace");
+        LOGGER.warn("Warning trace");
+        LOGGER.error("Error trace");
+        LOGGER.debug("Debug trace");
         return "person-add";
     }
 
     @PostMapping("/save")
     //capturamos el objeto person
     public ModelAndView save(@ModelAttribute("person") Person person){
+        LOGGER.info("Method: 'Person save' --params: '" + person + "'");
         ModelAndView modelAndView = new ModelAndView("person-save");
         modelAndView.addObject("person", person);
+        LOGGER.info("Template: 'person-save' --data: '" + person + "'");
 
         return modelAndView;
     }
