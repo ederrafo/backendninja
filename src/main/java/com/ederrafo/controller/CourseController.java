074391dc7923b187867ca8c91432a970e5f8ac7e
Eder Rafo Jose Pariona Espinhal <ederrafo@gmail.com>
2020-02-02 23:20:15 -0500

    agregue lectura de excel de cursos para grabar en la bd

--- src/main/java/com/ederrafo/controller/CourseController.java
+++ src/main/java/com/ederrafo/controller/CourseController.java
@@ -3,18 +3,21 @@ package com.ederrafo.controller;
 
 import com.ederrafo.entity.Course;
 import com.ederrafo.service.CourseService;
-import com.ederrafo.service.CourseServiceImpl;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.omg.CORBA.PUBLIC_MEMBER;
+import org.apache.poi.ss.usermodel.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.io.File;
+import java.math.BigDecimal;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Iterator;
 
 @Controller
 @RequestMapping("/courses")
@@ -53,4 +56,65 @@ public class CourseController {
         courseService.store(course);
         return  "redirect:/courses/all";
     }
+
+    @GetMapping("/addUpload")
+    public String addUpload()
+    {
+
+        return "courses/addUpload";
+    }
+
+    @PostMapping("/upload")
+    // public String uploadRead(Model model, MultipartFile multipartFile) throws IOException {
+
+    public String uploadRead(@RequestParam("file") MultipartFile multipartFile) throws Exception{
+
+        String nameofCurrentMethod = new Throwable().getStackTrace()[0].getMethodName();
+        LOG.info("From ->" + nameofCurrentMethod);
+        Path tempDir = Files.createTempDirectory("");
+        File tempFile = tempDir.resolve(multipartFile.getOriginalFilename()).toFile();
+
+        //Workbook workbook = WorkbookFactory.create(tempFile);
+        Workbook workbook = WorkbookFactory.create(multipartFile.getInputStream());
+
+        LOG.info("Workbook has  " + workbook.getNumberOfSheets() + " sheets :");
+
+        /*
+           =============================================================
+           Iterating over all the sheets in the workbook (Multiple ways)
+           =============================================================
+        */
+
+        Sheet sheet = workbook.getSheetAt(0);
+        DataFormatter dataFormatter = new DataFormatter();
+        Iterator<Row> rowIterator = sheet.rowIterator();
+        rowIterator.next();
+
+        for (int i = 1; i < sheet.getPhysicalNumberOfRows(); i++) {
+            Row row = sheet.getRow(i);
+            LOG.info("name  " + row.getCell(0).getStringCellValue() + "\t");
+            LOG.info("description  " + row.getCell(1).getStringCellValue() + "\t");
+            LOG.info("hours  " + row.getCell(2).getNumericCellValue () + "\t");
+            LOG.info("price  " + row.getCell(3).getNumericCellValue() + "\t");
+            Course course = new Course();
+            course.setName(row.getCell(0).getStringCellValue());
+            course.setDescription(row.getCell(1).getStringCellValue());
+            course.setPrice(new BigDecimal(row.getCell(3).getNumericCellValue()));
+            course.setHours((int) row.getCell(2).getNumericCellValue ());
+            courseService.store(course);
+
+        }
+
+
+
+        workbook.close();
+
+
+
+
+        return "courses/uploadRead";
+
+
+    }
+
 }
--- src/main/java/com/ederrafo/controller/ErrorsController.java
+++ src/main/java/com/ederrafo/controller/ErrorsController.java
@@ -5,15 +5,15 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 // Le decimos a spring que esta clase se encarga de la captura de errores
-@ControllerAdvice
+//@ControllerAdvice
 public class ErrorsController {
 
     public static final String TEMPLATE = "error/500";
 
     // retornara la vista del error
     // get all tipo de excelciones
-    @ExceptionHandler(Exception.class)
-    public String error(){
-        return TEMPLATE;
-    }
+   // @ExceptionHandler(Exception.class)
+   // public String error(){
+   //     return TEMPLATE;
+   // }
 }
